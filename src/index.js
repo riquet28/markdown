a8c459fa4cbdb5d75d36c0b969a1cf8559580873
riquet28 <eric.merlin3@gmail.com>
2017-05-31 05:00:34 +0200

    Utiliser et modifier les states ok

--- src/index.js
+++ src/index.js
@@ -8,16 +8,33 @@ import './index.css';
 import { sampleText } from './sampleText';
 
 class App extends React.Component {
+
+  state = {
+    text: sampleText
+  };
+
+  editText = (event) => {
+    const text = event.target.value;
+    this.setState({ text });
+  };
+
   render() {
     return(
       <div className="container">
         <div className="row">
           <div className="col-sm-6">
-            <textarea rows="35" className="form-control" value={ sampleText }></textarea>
+            <textarea
+              value={ this.state.text }
+              rows="35"
+              className="form-control"
+              onChange={(e) => this.editText(e)}
+            >
+
+            </textarea>
           </div>
           <div className="col-sm-6">
             <div>
-              <h1>{ sampleText }</h1>
+              <h1>{ this.state.text }</h1>
             </div>
           </div>
         </div>

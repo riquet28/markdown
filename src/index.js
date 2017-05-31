05a31e37d4d0d0f070328fba80e800d318528872
riquet28 <eric.merlin3@gmail.com>
2017-05-31 04:39:23 +0200

    Importer du js ok

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { render } from 'react-dom';
 //CSS
 import './style/css/bootstrap.min.css';
 import './index.css';
+//JS PERSO
+import { sampleText } from './sampleText';
 
 class App extends React.Component {
   render() {
@@ -11,12 +13,13 @@ class App extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-sm-6">
-            <textarea rows="35" className="form-control" value="Tapez votre texte"></textarea>
+            <textarea rows="35" className="form-control" value={ sampleText }></textarea>
           </div>
           <div className="col-sm-6">
-            <h1>Résultats</h1>
+            <div>
+              <h1>{ sampleText }</h1>
+            </div>
           </div>
-
         </div>
       </div>
     )
